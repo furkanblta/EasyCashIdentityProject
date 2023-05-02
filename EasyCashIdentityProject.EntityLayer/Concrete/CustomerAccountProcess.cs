de296f8b29f409d62400ff3e78197d2fb43b1c63
Cenasis <Cenasis@DESKTOP-0ET354O>
2023-05-02 16:16:22 +0300

    Identity Kütüphanesinin Eklenmesi

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public int ProcessType { get; set; }
+
+        public decimal Amout { get; set; }
+
+        public DateTime ProcessDate { get; set; }
+
+
+
+    }
+}
